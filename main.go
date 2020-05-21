c84dfb0e322db64b15d651ad938dabeae51b85aa
Ahmed ElFakharany <a.elfakharany@activevideo.com>
2020-05-21 16:01:16 +0200

    Using a master template for the layout and other fixes

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/gorilla/mux"
 )
 
 //Ticket a struct that holds information to be displayed in our HTML file
@@ -35,16 +37,75 @@ func readJSON(f string) Tickets {
 	json.Unmarshal(byteValue, &tickets)
 	return tickets
 }
+func writeJSON(f string, t Tickets) {
+	file, _ := json.MarshalIndent(t, "", " ")
+	_ = ioutil.WriteFile(f, file, 0644)
+
+}
 
 //Go application entrypoint
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tickets := readJSON("tickets.json")
-		templates := template.Must(template.ParseFiles("templates/home.html"))
-		if err := templates.ExecuteTemplate(w, "home.html", tickets); err != nil {
+		templates := template.Must(template.ParseFiles("templates/layout.html", "templates/home.html"))
+		if err := templates.ExecuteTemplate(w, "layout.html", tickets); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	fmt.Println(http.ListenAndServe(":8080", nil))
+	r.HandleFunc("/tickets/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := mux.Vars(r)["id"]
+		tickets := readJSON("tickets.json")
+		var ticket Ticket
+		for _, t := range tickets.Tickets {
+			if t.ID == id {
+				ticket = t
+			}
+		}
+		if ticket != (Ticket{}) {
+			templates := template.Must(template.ParseFiles("templates/layout.html", "templates/ticket.html"))
+			if err := templates.ExecuteTemplate(w, "layout.html", ticket); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+		} else {
+			http.Error(w, "The requested resource could not be found", http.StatusNotFound)
+		}
+	})
+	r.HandleFunc("/tickets/delete/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := mux.Vars(r)["id"]
+		tickets := readJSON("tickets.json")
+		var newTickets Tickets
+		for i, t := range tickets.Tickets {
+			if t.ID == id {
+				newTickets.Tickets = append(tickets.Tickets[:i], tickets.Tickets[i+1:]...)
+				writeJSON("tickets.json", newTickets)
+			}
+		}
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	})
+	r.HandleFunc("/tickets/close/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := mux.Vars(r)["id"]
+		tickets := readJSON("tickets.json")
+		for i, t := range tickets.Tickets {
+			if t.ID == id {
+				tickets.Tickets[i].Status = "closed"
+				writeJSON("tickets.json", tickets)
+			}
+		}
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	})
+	r.HandleFunc("/tickets/open/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := mux.Vars(r)["id"]
+		tickets := readJSON("tickets.json")
+		for i, t := range tickets.Tickets {
+			if t.ID == id {
+				tickets.Tickets[i].Status = "open"
+				writeJSON("tickets.json", tickets)
+			}
+		}
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	})
+	//  Handle static content
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
+	fmt.Println(http.ListenAndServe(":8080", r))
 }
